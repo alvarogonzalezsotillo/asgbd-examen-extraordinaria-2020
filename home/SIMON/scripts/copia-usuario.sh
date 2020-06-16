1e72cb4d57e24297ed74da110399fd26876edb91
alvaro@asir2 <alvaro@asir2>
2020-06-16 10:09:46 +0200

    Snapshot a las mar jun 16 10:09:46 CEST 2020

--- home/SIMON/scripts/copia-usuario.sh
+++ home/SIMON/scripts/copia-usuario.sh
@@ -17,3 +17,17 @@ nuevo_usu(){
 	create user SIMON_$DATE identified by USUARIO;
 FDF
 }
+ 
+existe_usuario(){
+	sqlplus SIMON/P843acb9 <<HEREDOC | grep -i $1
+	select username from dba_users where username = '$1';
+
+HEREDOC
+}
+
+if existe_usuario
+then 
+	echo el usuario ya existe
+else
+	nuevo_usu
+fi 
