790bf6dcab7b0f74dee191d71b5ef66e303d2688
alvaro@asir2 <alvaro@asir2>
2020-06-16 10:10:47 +0200

    Snapshot a las mar jun 16 10:10:47 CEST 2020

--- home/MBA/scripts/copia-usuario.sh
+++ home/MBA/scripts/copia-usuario.sh
@@ -2,4 +2,27 @@
 #export PATH=$PATH:$ORACLE_HOME/bin
 #export ORACLE_SID=XE
 
+existe_usuario(){
+HHMM=`
+sqlplus -S MBA/P32628c8 << HEREDOC                                   
+	 set server output on
+        declare  
+                exis integer;
+                cdo varchar(25);
+        being                   
+                select count(*) into exis from dba_users where upper(username)='${$│
+                if (exis = 0) then 
+                        dbms_output.put_line{'Creando Usuario: $1 con contraseña: $│
+                        execute inmediate 'create user MBA_$1 identified by $1';   │
+                        execute inmediate 'grant connect, resource to $1';         │
+                        select username into cdo from dba_users where upper(userna$│
+                        dbms_output.put_line{'Usuario creado: '|| creado);         │
+                else                                                               │
+                        dbms_output.put_line{'Ya existe el usuario: $1'};          │
+                end if;                                                            │
+        end;                                                                       │
+        /                                                                          │
+        HEREDOC                                                    
+}
 
+existe_usuario
