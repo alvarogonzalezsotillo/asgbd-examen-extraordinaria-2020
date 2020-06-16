399cc570cc620c96c0506221fc95a155123ef26a
alvaro@asir2 <alvaro@asir2>
2020-06-16 08:56:47 +0200

    Snapshot a las mar jun 16 08:56:47 CEST 2020

--- home/SIMON/scripts/crea-usuario-multas.sh
+++ home/SIMON/scripts/crea-usuario-multas.sh
@@ -6,10 +6,21 @@ export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
 crea_usuario(){
 	sqlplus SIMON/P834acb9 <<EOF
 	create user SIMON_'$1' identified by USUARIO;
-	grant conect, resource, create to '$1';
+	grant connect, resource, create to '$1';
 EOF
+}
+
+existe_usuario(){
+	sqlplus SIMON/P834acb9 <<EOF
+	select username from dba_users where username = 'SIMON_'$1'';
+FDF
 }
 
 if [ $# -eq 1 ]
 then
+	if existe_usuario
+	then
 		
+else 
+	echo necesito un parametro
+fi
